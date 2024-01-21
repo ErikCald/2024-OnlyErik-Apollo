b360b8a9b0f0c0e24f7b4461547930b9b2eecbe8
ErikCald <erikcaell@gmail.com>
2024-01-21 17:26:24 -0500

    Update ArmSubsystem.java

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -7,8 +7,32 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
+    private static ArmSubsystem INSTANCE = null;
+
+    private final CANSparkMax m_spark;
+    private final SparkMaxPIDController m_sparkPid;
+    private final SparkMaxAbsoluteEncoder m_encoder;
+
+    private final ProfiledExternalPIDController m_profiledPid;
+
+    private final PIDController m_simPid;
+    private boolean m_runPidSim = false;
+
+    private SimpleMotorFeedforward m_feedforward;
+
+    private double lastSpeed = 0;
+    private double lastFFVoltage = 0;
+
+    private DoublePublisher pubPos, pubVel, pubPosSet;
+    private DoublePublisher pubPosProSet, pubVelProSet, pubAccelSet;
+
     /** Creates a new ArmSubsystem. */
-    public ArmSubsystem() {}
+    public ArmSubsystem() {
+        /*
+         * Setup CANSparkMaxs
+         */
+        
+    }
 
     @Override
     public void periodic() {
